4df735d5d9d5fbc3228fd468bbd79c02abf9757c
Cláudio Alves <cmgalves@gmail.com>
2022-06-22 20:33:09 -0300

    alteração do endereço rest

--- src/app/shared/funcs/funcs.service.ts
+++ src/app/shared/funcs/funcs.service.ts
@@ -106,7 +106,8 @@ export class funcsService {
     let url = '';
 
     // url = `http://10.3.0.204:8095/DEV_REST/AJUST_EMP`
-    url = `http://10.3.0.204:8095/REST/AJUST_EMP`
+    // url = `http://10.3.0.204:8095/REST/AJUST_EMP`
+    url = `http://10.3.0.204:8090/REST/AJUST_EMP`
    
     return this._http.put(url, obj)
       .map((response: Response) => response.json());
